ed85931161c92b4d36f981c4220f5d57d206fcae
Ocholla-T <tonnyocholla8@gmail.com>
2021-10-09 14:59:02 +0300

    adding routing for the app

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import routes from './api/index.js';
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import routes from './api/index.js'
 
-dotenv.config({ path: './config/.env' });
-const app = express();
+dotenv.config({ path: './config/.env' })
+const app = express()
 
 // connection to database
-mongoose.connect(process.env.DB_CONNECTION_URL, () => console.log('connected to database'));
+mongoose.connect(process.env.DB_CONNECTION_URL, () => console.log('connected to database'))
 
 // global middleware
 app.use(
   express.urlencoded({
     extended: true,
   }),
-);
-app.use(express.json());
+)
+app.use(express.json())
 
-// routing for REST functions
-routes(app);
+// routing for the app
+app.use('/api/todo', routes)
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.listen(PORT, () => `app has started on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`app has started on port ${port}`)
+})
