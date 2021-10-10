a2bd91464c00319d88e178d2488c66e24e56c56b
Ocholla-T <tonnyocholla8@gmail.com>
2021-10-10 20:19:34 +0300

    added comments for jsdoc

--- tests/database.js
+++ tests/database.js
@@ -3,18 +3,27 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 
 let mongo
 
+/**
+ * connect to in memory server database
+ */
 async function connect() {
   mongo = await MongoMemoryServer.create()
   const uri = mongo.getUri()
   await mongoose.connect(uri)
 }
 
+/**
+ * drop the database and drop the connection
+ */
 async function closeDatabase() {
   await mongoose.connection.dropDatabase()
   await mongoose.connection.close()
   await mongo.stop()
 }
 
+/**
+ * clear the database, remove the data
+ */
 async function clearDatabase() {
   const collections = mongoose.connection.collections
   for (const key in collections) {
