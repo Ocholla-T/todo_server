97287997800a7590787c883ac48619bfe14692e3
Ocholla-T <tonnyocholla8@gmail.com>
2021-10-10 20:22:02 +0300

    edited to receive the named params on createTodo method

--- tests/createTodo.test.js
+++ tests/createTodo.test.js
@@ -1,5 +1,4 @@
 // @ts-nocheck
-import { MongoMemoryServer } from 'mongodb-memory-server'
 import { createTodo } from '../services/createTodo.js'
 import Todo from '../models/Todo.js'
 import db from './database.js'
@@ -8,17 +7,23 @@ beforeAll(async () => await db.connect())
 afterEach(async () => await db.clearDatabase())
 afterAll(async () => await db.closeDatabase())
 
-describe('Create a todo when: ', () => {
-  test('new todo', async () => {
-    const { todoID } = await createTodo('test todo', false)
+describe('Creating a todo: ', () => {
+  /**
+   * test if todo is created
+   */
+  test('should create a new todo', async () => {
+    const { todoID } = await createTodo({ content: 'test todo', isActive: false })
     const todo = await Todo.findById(todoID)
 
     expect(todo.content).toBe('test todo')
     expect(typeof todo.meta.isActive === 'boolean').toBeTruthy()
   })
+  /**
+   * test for invalid inputs(types)
+   */
   test('invalid input', async () => {
-    await expect(createTodo('first todo', 'im invalid')).rejects.toThrow()
-    await expect(createTodo('first todo', -1)).rejects.toThrow()
-    await expect(createTodo('first todo', {})).rejects.toThrow()
+    await expect(createTodo({ content: 'first todo', isActive: 'im invalid' })).rejects.toThrow()
+    await expect(createTodo({ content: true, isActive: -1 })).rejects.toThrow()
+    await expect(createTodo({ content: 'first todo', isActive: {} })).rejects.toThrow()
   })
 })
