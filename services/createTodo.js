3f1833cbfa3c869233702976e27d6324cfc654f5
Ocholla-T <tonnyocholla8@gmail.com>
2021-10-10 20:20:30 +0300

    changed parameters to named params

--- services/createTodo.js
+++ services/createTodo.js
@@ -1,25 +1,26 @@
-import Todo from '../models/Todo.js';
+import Todo from '../models/Todo.js'
 
 /**
  *  creates a new todo and saves it to the database
- * @param {String} content - todo content
- * @param {boolean} isActive - is the todo active or completed
+ * @param {Object} todo - the todo to be created
+ * @param {String} todo.content - the todo's content
+ * @param {boolean} todo.isActive - is the todo active or completed
  * @returns {Promise} todoID
  */
 
-export async function createTodo(content, isActive) {
+export async function createTodo({ content, isActive }) {
   try {
     const newTodo = new Todo({
       content,
       meta: {
         isActive,
       },
-    });
+    })
 
-    await newTodo.save();
+    await newTodo.save()
 
-    return { todoID: newTodo._id };
+    return { todoID: newTodo._id }
   } catch (error) {
-    throw error;
+    throw error
   }
 }
