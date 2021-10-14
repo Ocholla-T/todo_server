b046c4bd2bc11dc30ba006e65d9ef880cf7795c3
Ocholla-T <tonnyocholla8@gmail.com>
2021-10-14 13:13:40 +0300

    added unit test for deleteOneTodo method

---
+++ tests/deleteOneTodo.test.js
@@ -0,0 +1,27 @@
+// @ts-nocheck
+import db from './database.js'
+import Todo from '../models/Todo.js'
+import { deleteOneTodo } from '../services/deleteOneTodo.js'
+import { createTodo } from '../services/createTodo.js'
+
+beforeAll(() => db.connect())
+afterEach(() => db.clearDatabase())
+afterAll(() => db.closeDatabase())
+
+test('should delete one todo', async () => {
+  let todoOneID = (await createTodo({ content: 'test todo 1', isActive: true })).toString()
+
+  expect(await Todo.findById(todoOneID)).toBeTruthy()
+  await deleteOneTodo({ _id: todoOneID })
+  expect(await Todo.findById(todoOneID)).toBeFalsy()
+})
+
+test(`should throw error if id doesn't match ObjectId format`, async () => {
+  let todoID = '6165d512025ffd55991e122'
+
+  try {
+    await deleteOneTodo({ _id: todoID })
+  } catch (error) {
+    expect(error.message).toEqual('The _id is not a valid ObjectId')
+  }
+})
